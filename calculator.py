56bbf5d33c3775f506b67fa5535b432f1491720a
PerryXie123 <perryxie.nz@gmail.com>
2022-09-07 13:48:47 +1200

    pushtest

--- calculator.py
+++ calculator.py
@@ -18,7 +18,7 @@ print("welcome to the calculator")
 listSign = ["+", "-", "*", "/"]
 
 number1 = input("number 1: ")
-while not number1.replace('.','',1).isdigit():
+while not number1.replace('.', '', 1).isdigit():
     print("you idiot")
     number1 = input("please put a number this time: ")
 
@@ -28,7 +28,7 @@ while sign not in listSign:
     sign = input("please put a sign this time: ")
 
 number2 = input("number 2: ")
-while not number2.replace('.','',1).isdigit() or float(number2) == 0:
+while not number2.replace('.', '', 1).isdigit() or float(number2) == 0:
     print("you idiot")
     number2 = input("please put a number this time: ")
 
@@ -37,9 +37,9 @@ number2 = float(number2)
 
 
 if sign == "+":
-    print (number1+number2)
-elif sign == "-": 
-    print(number1-number2)
+    print(number1+number2)
+elif sign == "-":
+    print(number1 - number2)
 elif sign == "*":
     print(number1*number2)
 elif sign == "/" and number2 == 0:
@@ -47,4 +47,3 @@ elif sign == "/" and number2 == 0:
     number2 = input("please put a number thats not zero: ")
 elif sign == "/":
     print(number1/number2)
-
